33085dd30d9a82e71d5fb0e8bb8444ae4f49d70d
venturk <33842147+venturk@users.noreply.github.com>
2020-06-04 09:16:03 +0300

    Updates

---
+++ precache-manifest.17fc8793702756fc6b3a1c02fe1ba13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf786eca395eaab4728427e7eca181fd",
+    "url": "/PoGo-BIU-QR-codes/index.html"
+  },
+  {
+    "revision": "434cbb07e4a31fcd8c9b",
+    "url": "/PoGo-BIU-QR-codes/static/css/main.9bc23433.chunk.css"
+  },
+  {
+    "revision": "03c84a226d7123874e95",
+    "url": "/PoGo-BIU-QR-codes/static/js/2.66e998be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PoGo-BIU-QR-codes/static/js/2.66e998be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434cbb07e4a31fcd8c9b",
+    "url": "/PoGo-BIU-QR-codes/static/js/main.fa5b8806.chunk.js"
+  },
+  {
+    "revision": "3112ed747005a891e575",
+    "url": "/PoGo-BIU-QR-codes/static/js/runtime-main.e274fa4d.js"
+  }
+]);
